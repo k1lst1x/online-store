e78fd02b31575358c05358d8aee17d182644c595
saint <damir.dzhubaliev@gmail.com>
2025-03-24 07:53:31 +0600

    updated requirements.txt, login and admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
+from flask_admin.menu import MenuLink
 from flask import redirect, url_for, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -36,3 +37,17 @@ admin = Admin(name="Admin Panel", template_mode="bootstrap4")
 admin.add_view(AdminModelView(Product, db.session, name="Товары"))
 admin.add_view(AdminModelView(Feedback, db.session, name="Обратная связь"))
 admin.add_view(UserAdmin(User, db.session, name="Пользователи"))  # Используем кастомный UserAdmin
+
+class LoginMenuLink(MenuLink):
+
+    def is_accessible(self):
+        return not current_user.is_authenticated 
+
+
+class LogoutMenuLink(MenuLink):
+
+    def is_accessible(self):
+        return current_user.is_authenticated             
+
+admin.add_link(LogoutMenuLink(name='Logout', category='', url="/auth/logout"))
+admin.add_link(LoginMenuLink(name='Login', category='', url="/auth/login"))
